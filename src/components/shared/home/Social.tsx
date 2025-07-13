b05dfb46fef08ac9ca98fc450823ed64e046f5f5
abdelkabir <abdkabirneami@gmail.com>
2025-07-13 12:59:38 +0100

    UPD social media links

--- src/components/shared/home/Social.tsx
+++ src/components/shared/home/Social.tsx
@@ -2,14 +2,14 @@ export default function Social() {
     return (
         <div className="home__social">
             <a
-                href="https://www.instagram.com/abdelkabir_ne/"
+                href="https://www.instagram.com/abdelkabir_neami/"
                 className="home__social-icon"
                 target="_blank"
             >
                 <i className="uil uil-instagram"></i>
             </a>
             <a
-                href="https://www.linkedin.com/in/abdelkabir-neami-3a4143257/"
+                href="https://www.linkedin.com/in/abdelkabir-neami/"
                 className="home__social-icon"
                 target="_blank"
             >
